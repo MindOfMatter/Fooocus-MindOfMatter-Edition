87018e049c61d82f86f16f653deaf0cf17cd6185
MindOfMatter <martineaum22@outlook.com>
2024-02-07 01:51:32 -0500

    add last generation (with update all preset cmd)

---
+++ update_runs.ps1
@@ -0,0 +1,73 @@
+clear
+
+# Change to the script's directory
+$currentPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+# Run the last_preset_generator.ps1 script
+$lastPresetGeneratorPath = Join-Path -Path $currentPath -ChildPath "last_preset_generator.ps1"
+if (Test-Path -Path $lastPresetGeneratorPath) {
+    & $lastPresetGeneratorPath
+} else {
+    Write-Host "last_preset_generator.ps1 not found at path: $lastPresetGeneratorPath"
+}
+
+$scriptPath = Split-Path -Parent -Path $currentPath
+Set-Location -Path $scriptPath
+
+# Listing .json files in the specified directory
+$presetsPath = "$scriptPath\Fooocus\presets"
+$jsonFiles = Get-ChildItem -Path $presetsPath -Filter "*.json"
+
+# Iterate over each .json file
+foreach ($file in $jsonFiles) {
+    # Extract the preset name (without extension)
+    $preset = [System.IO.Path]::GetFileNameWithoutExtension($file.FullName)
+
+    # Check and create run_$preset.bat if not exists
+    $batFilePath = "$scriptPath\run_$preset.bat"
+    if (-not (Test-Path -Path $batFilePath)) {
+        $batContent = "@echo off`r`ncd /d %~dp0`r`n`r`n.\python_embeded\python.exe -s Fooocus\entry_with_update.py --preset $preset`r`npause"
+        Set-Content -Path $batFilePath -Value $batContent
+    }
+}
+
+# Print all .json files with an index
+for ($i = 0; $i -lt $jsonFiles.Count; $i++) {
+    Write-Host "$($i+1): $($jsonFiles[$i].Name)"
+}
+
+# Find the index of last.json
+$userChoice = 0
+for ($i = 0; $i -lt $jsonFiles.Count; $i++) {
+    if ($jsonFiles[$i].Name -eq "last.json") {
+        $userChoice = $i + 1
+        break
+    }
+}
+
+# overwrite it to 0 if choose from prompt (not default last json)
+#[int]$userChoice = 0
+
+# User input to choose a file
+if ($userChoice -eq 0) {
+    while ($userChoice -lt 1 -or $userChoice -gt $jsonFiles.Count) {
+        $userChoice = Read-Host "Enter the number of the preset you want to choose"
+    }
+}
+
+# Extract the chosen file name (without extension)
+$chosen_preset = [System.IO.Path]::GetFileNameWithoutExtension($jsonFiles[$userChoice - 1].FullName)
+
+# Display the chosen preset
+Write-Host "Running Fooocus with chosen preset: $chosen_preset"
+
+# Build the batch file name
+$batchFileName = "run_$chosen_preset.bat"
+
+# Check if the batch file exists
+if (Test-Path $batchFileName) {
+    # Run the batch file in a new Command Prompt window
+    Start-Process "cmd.exe" -ArgumentList "/c $batchFileName" -WindowStyle Normal
+} else {
+    Write-Host "Batch file not found: $batchFileName"
+}
